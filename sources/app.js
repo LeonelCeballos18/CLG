4520408839b70677db5bc41fb487c22d2cc98434
LeonelCeballos18 <lceballos5@ucol.mx>
2022-10-31 12:44:09 -0600

    AumentarReducir

--- sources/app.js
+++ sources/app.js
@@ -10,7 +10,7 @@ update()
 //Botonera
 
 function botonera(){
-    let pSize = 12;
+let pSize = 12;
 let hSize = 33;
 var textSize = document.getElementById('size');
 let tagP = document.getElementsByTagName('p');
@@ -20,17 +20,18 @@ const btnAumentar = document.querySelector('.add');
 btnAumentar.addEventListener('click', ()=>{
     pSize+=2;
     hSize+=2;
-    if(pSize<48){
-        console.log(pSize);
+    if(pSize<38){
         document.querySelector('#size').value = pSize;
-        var texto = document.querySelector(".texto");
-        texto.style.fontSize = pSize + 'px';
-        for(let i=0; i<tagP.length; i++){
-            document.getElementsByTagName('p')[i].style.fontSize = pSize;
-        }
-        for(let x=0; x<tagH.length; x++){
-            document.getElementsByTagName('h1')[x].style.fontSize = hSize;
-        }
+        var texto = document.querySelectorAll(".texto");
+        texto.forEach((etiquetaP, index) =>{
+            etiquetaP.style.fontSize = pSize + 'px';
+        })
+    }
+    if(hSize<71){
+        var h = document.querySelectorAll(".western");
+        h.forEach((etiquetaH, index) =>{
+            etiquetaH.style.fontSize = hSize + 'px';
+        })
     }
 })
 
@@ -41,28 +42,35 @@ btnReducir.addEventListener('click', ()=>{
     if(pSize>0){
         console.log(pSize);
         document.querySelector('#size').value = pSize;
-        document.querySelector(".texto").style.fontSize = pSize;
-        for(let i=0; i<tagP.length; i++){
-            document.getElementsByTagName('p')[i].style.fontSize = pSize;
-        }
-        for(let x=0; x<tagH.length; x++){
-            document.getElementsByTagName('h1')[x].style.fontSize = hSize;
-        }
+        var texto = document.querySelectorAll(".texto");
+        texto.forEach((etiquetaP, index) =>{
+            etiquetaP.style.fontSize = pSize + 'px';
+        })
+    }
+    if(hSize>21){
+        var h = document.querySelectorAll(".western");
+        h.forEach((etiquetaH, index) =>{
+            etiquetaH.style.fontSize = hSize + 'px';
+        })
     }
 })
 
 textSize.addEventListener('keypress', (event)=>{
     if (event.key === "Enter"){
-        if(textSize.valueAsNumber > 2 || textSize.valueAsNumber < 48){
+        if(parseInt(textSize.value, 10) > 2 || parseInt(textSize.value, 10) < 48){  //No funciona
+            console.log(2);
             pSize = textSize.valueAsNumber;
             hSize = textSize.valueAsNumber + 21;
-            console.log(hSize);
-            for(let i=0; i<tagP.length; i++){
-                document.getElementsByTagName('p')[i].style.fontSize = pSize;
-            }
-            for(let x=0; x<tagH.length; x++){
-                document.getElementsByTagName('h1')[x].style.fontSize = hSize;
-            }
+            console.log(pSize);
+            document.querySelector('#size').value = pSize;
+            var texto = document.querySelectorAll(".texto");
+            texto.forEach((etiquetaP, index) =>{
+                etiquetaP.style.fontSize = pSize + 'px';
+            })
+            var h = document.querySelectorAll(".western");
+            h.forEach((etiquetaH, index) =>{
+                etiquetaH.style.fontSize = hSize + 'px';
+            })
         }
     }
 })
